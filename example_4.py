de1ac53a53fa0ab52dc32f835e7bcb43d66f990c
Vetrov Dmitriy <13im@mail.ru>
2023-11-14 00:45:02 +0300

    Commit

--- example_4.py
+++ example_4.py
@@ -3,20 +3,30 @@
 """
 import asyncio
 import aiohttp
+from time import perf_counter
 
 async def get_hystori(num_page: int):
     async with aiohttp.ClientSession() as session:
-        async with session.get(f'https://eku.ru/category/story/?page={str(num_page)}', raise_for_status=True) as rec:
-            # response = await rec.read()
-            dc_res = {'num_page': num_page, 'status_code': rec.status}
+        # async with session.get(f'https://eku.ru/category/story/?page={str(num_page)}', raise_for_status=True) as rec:
+        try:
+            async with session.get(f'https://fortuna-dom.ru/{num_page}', raise_for_status=True) as rec:
+                # response = await rec.read()
+                dc_res = {'num_page': num_page, 'status_code': rec.status}
+        except:
+            dc_res = {'num_page': num_page, 'status_code': 'Error'}
+        finally:
             return dc_res
 
+
 async def main():
-    ls_cur = [get_hystori(i) for i in range(1, 200)]
+    ls_cur = [get_hystori(i) for i in range(1, 500)]
     for cur in asyncio.as_completed(ls_cur):
         result = await cur
         print(result)
 
 
 if __name__ == '__main__':
+    t_1 = perf_counter()
     asyncio.run(main())
+    print(perf_counter() - t_1)
+    # 11.2, 10.6, 10.34, 10.2, 13.14 = 10.71
--- example_5.py
+++ example_5.py
@@ -5,10 +5,11 @@
 если содержимое ответа не читается или соединение ответа не освобождается вручную.
 
 Ожидаемое поведение
-Ответное соединение должно быть разорвано автоматически. --  await response.connection.release()
+!!! Ответное соединение должно быть разорвано автоматически. --  await response.connection.release()
 """
 import asyncio
 import aiosonic
+from time import perf_counter
 
 async def get_hystori(num_page: int):
     async with aiosonic.HTTPClient(verify_ssl=False) as client:
@@ -18,11 +19,14 @@ async def get_hystori(num_page: int):
         return dc_res
 
 async def main():
-    ls_cur = [get_hystori(i) for i in range(1, 10)]
+    ls_cur = [get_hystori(i) for i in range(1, 100)]
     for cur in asyncio.as_completed(ls_cur):
         result = await cur
         print(result)
 
 
 if __name__ == '__main__':
+    t_1 = perf_counter()
     asyncio.run(main())
+    print(perf_counter() - t_1)
+    # 10.48, 10.29, 10.78, 14.12, 10.75 = 10.67
