275e54c364ca7f1a8b0618dfcc5e300dbeac2456
VetrovDmitry007 <13im@mail.ru>
2023-11-15 11:06:17 +0300

    Add fetch_httpx.py

--- fetch_httpx.py
+++ fetch_httpx.py
@@ -1,8 +1,8 @@
 import base64
 import asyncio
-import aiosonic
 from tqdm import tqdm
 from typing import Optional, Tuple, Dict, Any
+import httpx
 
 DEFAULT_CONNECT_TIMEOUT = 10
 DEFAULT_REQUEST_TIMEOUT = 3600
@@ -10,7 +10,7 @@ DEFAULT_BUFFER_SIZE = 128 * 1024
 DEFAULT_MAX_REDIRECTS = 5
 
 
-async def fetch_sonic(
+async def fetch_httpx(
         url: str,
         method: str = "GET",
         headers: Dict = None,
@@ -20,7 +20,7 @@ async def fetch_sonic(
         # resolver=resolve,
         # max_buffer_size=DEFAULT_BUFFER_SIZE,
         follow_redirects: bool = False,
-        # max_redirects=DEFAULT_MAX_REDIRECTS, # request() -> max_redirects = 30
+        max_redirects=DEFAULT_MAX_REDIRECTS,
         validate_cert: bool = False,  # config.http_client.validate_certs,
         allow_proxy: bool = False,
         proxies=None,
@@ -40,24 +40,24 @@ async def fetch_sonic(
             headers = header_aut
     # Checking proxy usage
     if proxies and allow_proxy:
-        proxy = aiosonic.Proxy(host=proxies)
+        proxy = httpx.Proxy(url=proxies)
     else:
         proxy = None
     # Sending a request
-    async with aiosonic.HTTPClient(verify_ssl=validate_cert, proxy=proxy) as client:
-        timeouts = aiosonic.Timeouts(sock_read=request_timeout, sock_connect=connect_timeout)
+    async with httpx.AsyncClient(proxies=proxy, verify=validate_cert, max_redirects=max_redirects) as client:
+        timeouts = httpx.Timeout(read=request_timeout, connect=connect_timeout, pool=None, write=None)
         if method == "GET":
-            response = await client.get(url, headers=headers, timeouts=timeouts, follow=follow_redirects)
+            response = await client.get(url, headers=headers)
         elif method == "POST":
-            response = await client.post(url, headers=headers, timeouts=timeouts, data=body, follow=follow_redirects)
+            response = await client.post(url, headers=headers, timeout=timeouts, data=body, follow_redirects=follow_redirects)
         elif method == "PUT":
-            response = await client.put(url, headers=headers, timeouts=timeouts,  data="" if body is None else body, follow=follow_redirects)
+            response = await client.put(url, headers=headers, timeout=timeouts,  data="" if body is None else body, follow_redirects=follow_redirects)
         elif method == "DELETE":
-            response = await client.delete(url, headers=headers, timeouts=timeouts, follow=follow_redirects)
+            response = await client.delete(url, headers=headers, timeout=timeouts, follow_redirects=follow_redirects)
         status = response.status_code
         headers = dict(response.headers)
-        body = response.body
-        await response.connection.release()
+        body = await response.aread()
+        # await response.connection.release()
     return (status, headers, body)
 
 
@@ -66,7 +66,7 @@ async def test_get():
     print('Start GET request.')
     url = 'https://httpbin.org/get'
     method = 'GET'
-    status, headers, body = await fetch_sonic(
+    status, headers, body = await fetch_httpx(
         url,
         method=method,
         connect_timeout=5,
@@ -80,7 +80,7 @@ async def test_post():
     print('Start POST request.')
     url = 'https://httpbin.org/post'
     method = 'POST'
-    status, headers, body = await fetch_sonic(
+    status, headers, body = await fetch_httpx(
         url,
         method=method,
         body= {'data_test': 'data_test'},
@@ -95,7 +95,7 @@ async def test_put():
     print('Start PUT request.')
     url = 'https://httpbin.org/put'
     method = 'PUT'
-    status, headers, body = await fetch_sonic(
+    status, headers, body = await fetch_httpx(
         url,
         method=method,
         body= {'data_test': 'data_test'},
@@ -110,11 +110,11 @@ async def test_delete():
     print('Start DELETE request.')
     url = 'https://httpbin.org/delete'
     method = 'DELETE'
-    status, headers, body = await fetch_sonic(
+    status, headers, body = await fetch_httpx(
         url,
         method=method,
         connect_timeout=5,
-        request_timeout=3600,
+        request_timeout=10,
         validate_cert=False,
     )
     print(f'{status=},\n{headers=},\n{body=}')
@@ -123,9 +123,9 @@ async def test_delete():
 
 async def main():
     await test_get()
-    # await test_post()
-    # await test_put()
-    # await test_delete()
+    await test_post()
+    await test_put()
+    await test_delete()
 
 if __name__ == '__main__':
     asyncio.run(main())
